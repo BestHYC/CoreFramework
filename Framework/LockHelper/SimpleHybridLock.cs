08f40510f1df01c85e047456ed5a8a4ab4dc21a9
0100851 <yichao.hong@onesmart.org>
2019-06-25 17:41:10 +0800

    提交自用核心框架,避免重复造轮子
    Signed-off-by: 0100851 <yichao.hong@onesmart.org>

---
+++ Framework/LockHelper/SimpleHybridLock.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Framework.LockHelper
+{
+    public class SimpleHybridLock : IDisposable
+    {
+        private Int32 m_waiters = 0;
+        private AutoResetEvent m_waiterLock = new AutoResetEvent(false);
+        public void Enter()
+        {
+            if (Interlocked.Increment(ref m_waiters) == 1)
+                return;
+            m_waiterLock.WaitOne();
+        }
+        public void Leave()
+        {
+            if (Interlocked.Decrement(ref m_waiters) == 0)
+                return;
+            m_waiterLock.Set();
+        }
+        public void Dispose()
+        {
+            m_waiterLock.Dispose();
+        }
+    }
+}
