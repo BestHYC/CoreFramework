643f9f6554364cb5739767622ebb3f57af45c29e
Best_Hong <970973742@qq.com>
2020-05-18 11:08:35 +0800

    添加TaskExtension以及ApiResult扩展,做全局API返回使用
    Signed-off-by: 'Best_Hong' <'970973742@qq.com'>

---
+++ Framework/ApiResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Framework
+{
+    public enum ResultStatusEnum
+    {
+        Success = 0, Error = 1
+    }
+    public class ApiResult
+    {
+        public ResultStatusEnum Status { get; set; }
+        public String Message { get; set; }
+    }
+    public class ApiResult<T> : ApiResult
+    {
+        public T Data { get; set; }
+    }
+}
