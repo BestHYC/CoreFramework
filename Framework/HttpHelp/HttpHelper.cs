a37f4ce0d956a71dfcc1ad2968fd5df61a6563d2
Best_Hong <970973742@qq.com>
2020-06-02 10:44:21 +0800

    修改命名空间至Framework
    Signed-off-by: 'Best_Hong' <'970973742@qq.com'>

--- Framework/HttpHelp/HttpHelper.cs
+++ Framework/HttpHelp/HttpHelper.cs
@@ -5,7 +5,7 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Framework.HttpHelp
+namespace Framework
 {
     public class HttpHelper
     {
--- Framework/ORM/RedisHelper/RedisHelper.cs
+++ Framework/ORM/RedisHelper/RedisHelper.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Framework.ORM
+namespace Framework
 {
     /// <summary>
     /// Redis 助手
