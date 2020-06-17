8336ab27fee4957d0bf065fac76fc5a2e6cfcd7f
Best_Hong <970973742@qq.com>
2020-06-17 18:02:54 +0800

    添加加密解密工具
    Signed-off-by: 'Best_Hong' <'970973742@qq.com'>

--- Framework/Secrets/SecretHelper.cs
+++ Framework/Secrets/SecretHelper.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Security.Cryptography;
 using System.Text;
 
@@ -57,6 +59,63 @@ namespace Framework
                 output.Append(current.ToString("X"));
             }
             return output.ToString();
+        }
+        /// <summary>
+        /// DES加密
+        /// </summary>
+        /// <param name="data">加密数据</param>
+        /// <param name="key">8位字符的密钥字符串</param>
+        /// <param name="iv">8位字符的初始化向量字符串</param>
+        /// <returns></returns>
+        public static string DESEncrypt(string data, string key, string iv)
+        {
+            if (String.IsNullOrWhiteSpace(key)) key = "";
+            if (String.IsNullOrWhiteSpace(iv)) iv = "";
+            byte[] byKey = System.Text.ASCIIEncoding.ASCII.GetBytes(key);
+            byte[] byIV = System.Text.ASCIIEncoding.ASCII.GetBytes(iv);
+
+            DESCryptoServiceProvider cryptoProvider = new DESCryptoServiceProvider();
+            int i = cryptoProvider.KeySize;
+            MemoryStream ms = new MemoryStream();
+            CryptoStream cst = new CryptoStream(ms, cryptoProvider.CreateEncryptor(byKey, byIV), CryptoStreamMode.Write);
+
+            StreamWriter sw = new StreamWriter(cst);
+            sw.Write(data);
+            sw.Flush();
+            cst.FlushFinalBlock();
+            sw.Flush();
+            return Convert.ToBase64String(ms.GetBuffer(), 0, (int)ms.Length);
+        }
+        /// <summary>
+        /// DES解密
+        /// </summary>
+        /// <param name="data">解密数据</param>
+        /// <param name="key">8位字符的密钥字符串(需要和加密时相同)</param>
+        /// <param name="iv">8位字符的初始化向量字符串(需要和加密时相同)</param>
+        /// <returns></returns>
+        public static T DESDecrypt<T>(string data, string key, string iv)
+        {
+            if (String.IsNullOrWhiteSpace(key)) key = "";
+            if (String.IsNullOrWhiteSpace(iv)) iv = "";
+            byte[] byKey = System.Text.ASCIIEncoding.ASCII.GetBytes(key);
+            byte[] byIV = System.Text.ASCIIEncoding.ASCII.GetBytes(iv);
+
+            byte[] byEnc;
+            try
+            {
+                byEnc = Convert.FromBase64String(data);
+            }
+            catch
+            {
+                return default(T);
+            }
+
+            DESCryptoServiceProvider cryptoProvider = new DESCryptoServiceProvider();
+            MemoryStream ms = new MemoryStream(byEnc);
+            CryptoStream cst = new CryptoStream(ms, cryptoProvider.CreateDecryptor(byKey, byIV), CryptoStreamMode.Read);
+            StreamReader sr = new StreamReader(cst);
+            String result = sr.ReadToEnd();
+            return JsonConvert.DeserializeObject<T>(result);
         }
     }
 }
