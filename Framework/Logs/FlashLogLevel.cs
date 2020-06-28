39904b76e08af04a952cd9aa3d4f14154f987e83
Best_Hong <970973742@qq.com>
2020-06-28 19:13:03 +0800

    修改日志文件为本地保存,并添加缓存日志
    Signed-off-by: 'Best_Hong' <'970973742@qq.com'>

---
+++ Framework/Logs/FlashLogLevel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Framework
+{
+    /// <summary>
+    /// 日志等级
+    /// </summary>
+    public enum FlashLogLevel
+    {
+        Debug,
+        Info,
+        Error,
+        Warn,
+        Fatal
+    }
+}
---
+++ Framework/Logs/FlashLogMessage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Framework
+{
+    /// <summary>
+    /// 日志内容
+    /// </summary>
+    public class FlashLogMessage
+    {
+        public string Message { get; set; }
+        public FlashLogLevel Level { get; set; }
+        public Exception Exception { get; set; }
+
+    }
+}
