f6ef7783b70b87c62962c5ab6fbfb5d6b81e3342
Best_Hong <970973742@qq.com>
2020-07-29 11:44:25 +0800

    提交状态码
    Signed-off-by: 'Best_Hong' <'970973742@qq.com'>

--- Framework/ApiResult.cs
+++ Framework/ApiResult.cs
@@ -17,4 +17,69 @@ namespace Framework
     {
         public T Data { get; set; }
     }
+    /// <summary>
+    /// 延伸的状态码数据
+    /// </summary>
+    public class ApiStatusCode
+    {
+        /// <summary>
+        /// 成功
+        /// </summary>
+        public const String Success = "00000";
+        /// <summary>
+        /// 登录已失效
+        /// </summary>
+        public const String LogInFail = "01001";
+        /// <summary>
+        /// 当前时间与服务器不一致
+        /// </summary>
+        public const String FailTime = "01002";
+        /// <summary>
+        /// 参数不能为空
+        /// </summary>
+        public const String NoArgument = "01008";
+        /// <summary>
+        /// 参数格式不正确
+        /// </summary>
+        public const String FailArgument = "01010";
+
+
+        /// <summary>
+        /// 不支持的支付方式
+        /// </summary>
+        public const String FailPayWay = "02001";
+        /// <summary>
+        ///  获取汇率异常
+        /// </summary>
+        public const String FailGetRate = "04011";
+        /// <summary>
+        ///  转账异常
+        /// </summary>
+        public const String FailTransefer = "04011";
+        /// <summary>
+        /// 链和币种相关系统异常
+        /// </summary>
+        public const String FailSystem = "05000";
+        /// <summary>
+        /// 不支持币种
+        /// </summary>
+        public const String FailCoinType = "05002";
+        /// <summary>
+        /// 无效地址
+        /// </summary>
+        public const String FailAddress = "05004";
+        /// <summary>
+        /// 系统内部错误
+        /// </summary>
+        public const String SystemInnerFail = "09000";
+        /// <summary>
+        /// 系统异常，请联系客服处理。
+        /// </summary>
+        public const String InnerFail = "09001";
+        /// <summary>
+        /// 系统异常，请联系客服处理。
+        /// </summary>
+        public const String UnKnowStatus = "09002";
+
+    }
 }
