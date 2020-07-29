66f4005a17144cbc0aeee61896807c1cfaf4dac2
Best_Hong <970973742@qq.com>
2020-07-29 16:26:58 +0800

    提交日志查看
    Signed-off-by: 'Best_Hong' <'970973742@qq.com'>

--- Framework/ApiResult.cs
+++ Framework/ApiResult.cs
@@ -25,72 +25,75 @@ namespace Framework
         /// <summary>
         /// 成功
         /// </summary>
-        public const String Success = "00000";
+        public const Int32 Success = 0;
         /// <summary>
         /// 登录已失效
         /// </summary>
-        public const String LogInFail = "01001";
+        public const Int32 LogInFail = 1001;
         /// <summary>
         /// 当前时间与服务器不一致
         /// </summary>
-        public const String FailTime = "01002";
+        public const Int32 FailTime = 1002;
         /// <summary>
         /// 参数不能为空
         /// </summary>
-        public const String NoArgument = "01008";
+        public const Int32 NoArgument = 1008;
         /// <summary>
         /// 参数格式不正确
         /// </summary>
-        public const String FailArgument = "01010";
+        public const Int32 FailArgument = 1010;
 
 
         /// <summary>
         /// 不支持的支付方式
         /// </summary>
-        public const String FailPayWay = "02001";
-        public const String TranceferError = "04000";
-        public const String ArgumentNull = "04001";
-        public const String FailPre = "04101";
+        public const Int32 FailPayWay = 2001;
+        public const Int32 TranceferError = 4000;
+        /// <summary>
+        /// 关键参数为空(参数不为空,但是关键参数为空)
+        /// </summary>
+        public const Int32 ArgumentNull = 4001;
+        public const Int32 FailPre = 4101;
         /// <summary>
         /// 重复订单号
         /// </summary>
-        public const String RepeatOrderId = "04102";
+        public const Int32 RepeatOrderId = 4102;
         /// <summary>
         /// 无效订单号
         /// </summary>
-        public const String FailOrderId = "04103";
+        public const Int32 FailOrderId = 4103;
         /// <summary>
         ///  获取汇率异常
         /// </summary>
-        public const String FailGetRate = "04011";
+        public const Int32 FailGetRate = 4011;
         /// <summary>
         ///  转账异常
         /// </summary>
-        public const String FailTransefer = "04011";
+        public const Int32 FailTransefer = 4011;
         /// <summary>
         /// 链和币种相关系统异常
         /// </summary>
-        public const String FailSystem = "05000";
+        public const Int32 FailSystem = 5000;
         /// <summary>
         /// 不支持币种
         /// </summary>
-        public const String FailCoinType = "05002";
+        public const Int32 FailCoinType = 5002;
         /// <summary>
         /// 无效地址
         /// </summary>
-        public const String FailAddress = "05004";
+        public const Int32 FailAddress = 5004;
         /// <summary>
         /// 系统内部错误
         /// </summary>
-        public const String SystemInnerFail = "09000";
+        public const Int32 SystemInnerFail = 9000;
         /// <summary>
         /// 系统异常，请联系客服处理。
         /// </summary>
-        public const String InnerFail = "09001";
+        public const Int32 InnerFail = 9001;
         /// <summary>
         /// 系统异常，请联系客服处理。
         /// </summary>
-        public const String UnKnowStatus = "09002";
+        public const Int32 UnKnowStatus = 9002;
 
     }
 }
